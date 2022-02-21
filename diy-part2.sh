aeaa7718c3f980014672a1d100dc00eeae19671f
Benjamin <benjaminx@gmail.com>
2022-02-21 12:00:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,8 +16,8 @@ sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 echo '###  ###'
 
 # echo '### Updates Theme Argon ###'
-# rm -rf package/lean/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 # echo '###  ###'
 
 echo '### 添加 R4S GPU 驱动 ###'
