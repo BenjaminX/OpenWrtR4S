58b6633f5eaa1ff520d827d1c50541c3296dabc5
Benjamin <benjaminx@gmail.com>
2022-08-15 19:57:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,30 +21,30 @@ rm -rf package/feeds/luci/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
 echo '###  ###'
 
-# echo '### 添加 R4S GPU 驱动 ###'
-# rm -rf package/kernel/linux/modules/video.mk
-# wget -P package/kernel/linux/modules/ https://raw.githubusercontent.com/immortalwrt/immortalwrt/master/package/kernel/linux/modules/video.mk
-# echo '###  ###'
+echo '### 添加 R4S GPU 驱动 ###'
+rm -rf package/kernel/linux/modules/video.mk
+wget -P package/kernel/linux/modules/ https://raw.githubusercontent.com/immortalwrt/immortalwrt/master/package/kernel/linux/modules/video.mk
+echo '###  ###'
 
-# echo '### 使用特定的优化 ###'
-# sed -i 's,-mcpu=generic,-mcpu=cortex-a72.cortex-a53+crypto,g' include/target.mk
-# cp -f $GITHUB_WORKSPACE/PATCH/mbedtls/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch package/libs/mbedtls/patches/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch
-# sed -i 's,kmod-r8169,kmod-r8168,g' target/linux/rockchip/image/armv8.mk
-# echo '###  ###'
+echo '### 使用特定的优化 ###'
+sed -i 's,-mcpu=generic,-mcpu=cortex-a72.cortex-a53+crypto,g' include/target.mk
+cp -f $GITHUB_WORKSPACE/PATCH/mbedtls/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch package/libs/mbedtls/patches/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch
+sed -i 's,kmod-r8169,kmod-r8168,g' target/linux/rockchip/image/armv8.mk
+echo '###  ###'
 
-# echo '### CacULE ###'
-# sed -i '/CONFIG_NR_CPUS/d' ./target/linux/rockchip/armv8/config-5.4
-# echo '
-# CONFIG_NR_CPUS=6
-#' >> ./target/linux/rockchip/armv8/config-5.4
-# echo '###  ###'
+echo '### CacULE ###'
+sed -i '/CONFIG_NR_CPUS/d' ./target/linux/rockchip/armv8/config-5.4
+echo '
+CONFIG_NR_CPUS=6
+' >> ./target/linux/rockchip/armv8/config-5.4
+echo '###  ###'
 
-# echo '### UKSM ###'
-# echo '
-# CONFIG_KSM=y
-# CONFIG_UKSM=y
-# ' >> ./target/linux/rockchip/armv8/config-5.4
-# echo '###  ###'
+echo '### UKSM ###'
+echo '
+CONFIG_KSM=y
+CONFIG_UKSM=y
+' >> ./target/linux/rockchip/armv8/config-5.4
+echo '###  ###'
 
 # echo '### IRQ 调优 ###'
 # sed -i '/set_interface_core 20 "eth1"/a\set_interface_core 8 "ff3c0000" "ff3c0000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
